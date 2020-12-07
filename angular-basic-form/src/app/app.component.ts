4c3bf5b56432eb54012e40aa9432b4de03451be0
Lea Dalibard <leadalibard@gmail.com>
2020-12-07 10:25:01 +0100

    Changing select to array with ngo for languages

--- angular-basic-form/src/app/app.component.ts
+++ angular-basic-form/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-name';
+  languages = [{name: 'Html'}, {name: 'CSS'}, {name: 'JS'}, {name: 'C#'}, {name: 'Python'}];
 }
+
+
+
+
