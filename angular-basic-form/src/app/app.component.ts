456a06f5a8ebe06414b52140b252083e6cbcd028
Lea Dalibard <leadalibard@gmail.com>
2020-12-08 09:40:47 +0100

    Step 13 completed button-> [disabled]=friend.form.invalid

--- angular-basic-form/src/app/app.component.ts
+++ angular-basic-form/src/app/app.component.ts
@@ -10,7 +10,6 @@ export class AppComponent {
   title = 'project-name';
   languages = [{name: 'Html'}, {name: 'CSS'}, {name: 'JS'}, {name: 'C#'}, {name: 'Python'}];
 
-  phonePattern="^(?:0|\(?\+33\)?\s?|0033\s?)[1-79](?:[\.\-\s]?\d\d){4}$"+"^(?:0|\(?\+32\)?\s?|0032\s?)[1-79](?:[\.\-\s]?\d\d){4}$";
 
 
   friendModel = new Friend(" ", " ", " ", " ", " ");
